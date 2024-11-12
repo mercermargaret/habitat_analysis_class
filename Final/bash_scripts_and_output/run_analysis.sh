2353ce40442127faedc9b9e9ef4dfc58d3f6cccb
Margaret Mercer <iammegmercer@gmail.com>
2024-11-12 16:15:43 -0700

    fixed walltime

--- Final/bash_scripts_and_output/run_analysis.sh
+++ Final/bash_scripts_and_output/run_analysis.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=test         # name of the job (make it short!)
 #SBATCH --partition=standard           # partition to be used (standard OR windfall)
 #SBATCH --account=jmalston          # hpc group name! (always jmalston)
-#SBATCH --time=48:00:00            # walltime (up to 10-00:00:00(240:00:00))
+#SBATCH --time=24:00:00            # walltime (up to 10-00:00:00(240:00:00))
 #SBATCH --nodes=4                  # number of nodes
 #SBATCH --ntasks-per-node=1        # number of tasks (i.e. parallel processes) to be started
 #SBATCH --cpus-per-task=1          # number of cpus required to run the script
