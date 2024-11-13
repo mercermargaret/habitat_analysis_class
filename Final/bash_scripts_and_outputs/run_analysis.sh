0ddecc7991e4edf63ecaabfd1262a4d7e080f943
Margaret Mercer <iammegmercer@gmail.com>
2024-11-12 17:33:24 -0700

    asdfasdf

--- Final/bash_scripts_and_outputs/run_analysis.sh
+++ Final/bash_scripts_and_outputs/run_analysis.sh
@@ -7,13 +7,13 @@
 #SBATCH --ntasks-per-node=1        # number of tasks (i.e. parallel processes) to be started
 #SBATCH --cpus-per-task=1          # number of cpus required to run the script
 #SBATCH --mem-per-cpu=32G         # memory required for process
-#SBATCH --array=0-1%2    	   # set number of total simulations and number that can run simultaneously (0-33%34)
+#SBATCH --array=0-1%2    	       # set number of total simulations and number that can run simultaneously (0-33%34)
 
 ml R gdal/3.8.5
 
 cd /home/u15/mmercer3/proj/habitat_analysis_class   # where executable and data is located
 
-list=(/home/u15/mmercer3/proj/habitat_analysis_class/Final/Bobcat_Individuals/range_resident/*.csv)
+list=(/home/u15/mmercer3/proj/habitat_analysis_class/Final/Model_Fit_Results/)
 
 date
 echo "Initiating script"
