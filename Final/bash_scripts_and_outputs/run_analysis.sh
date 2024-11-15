b551a249f1886427f978c50266fe55b5c66f1cd2
Margaret Mercer <mmercer3@wentletrap.hpc.arizona.edu>
2024-11-15 15:36:25 -0700

    successfully ran testrsfs

--- Final/bash_scripts_and_outputs/run_analysis.sh
+++ Final/bash_scripts_and_outputs/run_analysis.sh
@@ -3,10 +3,10 @@
 #SBATCH --partition=standard           # partition to be used (standard OR windfall)
 #SBATCH --account=jmalston          # hpc group name! (always jmalston)
 #SBATCH --time=24:00:00            # walltime (up to 10-00:00:00(240:00:00))
-#SBATCH --nodes=4                  # number of nodes
+#SBATCH --nodes=1                  # number of nodes
 #SBATCH --ntasks-per-node=1        # number of tasks (i.e. parallel processes) to be started
-#SBATCH --cpus-per-task=1          # number of cpus required to run the script
-#SBATCH --mem-per-cpu=32G         # memory required for process
+#SBATCH --cpus-per-task=8          # number of cpus required to run the script
+#SBATCH --mem-per-cpu=4G         # memory required for process
 #SBATCH --array=0-1%2    	       # set number of total simulations and number that can run simultaneously (0-33%34)
 
 ml R gdal/3.8.5
